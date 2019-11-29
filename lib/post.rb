866a2b165d8cb7e6b2574528e020b2cbb9b4cae8
Ken Goad <goadke@g.cofc.edu>
2019-11-29 00:04:25 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+class Post
+  attr_accessor :artist, :name, :all
+ 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @artist = artist
+    save
+  end
+ 
+  def save
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+end
